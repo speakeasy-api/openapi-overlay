94a5aa49b2e313701ecaf06f510183b07f85d1c3
Thomas Rooney <thomas@resiliencycollective.com>
2023-11-06 10:26:36 +0000

    chore: upgrade libopenapi (wip)

--- pkg/overlay/parse.go
+++ pkg/overlay/parse.go
@@ -3,7 +3,6 @@ package overlay
 import (
 	"fmt"
 	"github.com/pb33f/libopenapi/index"
-	"github.com/pb33f/libopenapi/resolver"
 	"gopkg.in/yaml.v3"
 	"io"
 	"os"
@@ -12,49 +11,72 @@ import (
 
 // Parse will parse the given reader as an overlay file.
 func Parse(path string) (*Overlay, error) {
+	// Note: this is TCR tinkering around: isn't working right now.
 	filePath, err := filepath.Abs(path)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get absolute path for %q: %w", path, err)
 	}
-
 	ro, err := os.Open(filePath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to open overlay file at path %q: %w", path, err)
+		return nil, fmt.Errorf("failed to open overlay file %q: %w", path, err)
+	}
+	yamlDec := yaml.NewDecoder(ro)
+	var yamlNode yaml.Node
+	err = yamlDec.Decode(&yamlNode)
+
+	cfg := index.CreateOpenAPIIndexConfig()
+	baseDir := filepath.Dir(filePath)
+	cfg.BasePath = baseDir
+
+	rolo := index.NewRolodex(cfg)
+
+	localFSConf := index.LocalFSConfig{
+		BaseDirectory: baseDir,
+		DirFS:         os.DirFS(baseDir),
 	}
 
-	var overlay Overlay
-	dec := yaml.NewDecoder(ro)
-	var rootNode yaml.Node
-
-	err = dec.Decode(&rootNode)
+	fileFS, err := index.NewLocalFSWithConfig(&localFSConf)
 	if err != nil {
 		return nil, err
 	}
+	rolo.AddLocalFS(baseDir, fileFS)
 
-	cfg := index.CreateOpenAPIIndexConfig()
-	cfg.BasePath = filepath.Dir(filePath)
-	idx := index.NewSpecIndexWithConfig(&rootNode, cfg)
-	referenceErrors := idx.GetReferenceIndexErrors()
-	if len(referenceErrors) > 0 {
-		msg := ""
-		for _, err := range referenceErrors {
-			msg += err.Error() + ";"
-		}
-		return nil, fmt.Errorf("error indexing spec: %s", msg)
+	remoteFS, _ := index.NewRemoteFSWithConfig(cfg)
+	rolo.AddRemoteFS("default", remoteFS)
+	rolo.SetRootNode(&yamlNode)
+	//f, err := rolo.Open(path)
+	//if err != nil {
+	//	return nil, fmt.Errorf("failed to open overlay file %q: %w", path, err)
+	//}
+	// Note: seems aggressive
+	err = rolo.IndexTheRolodex()
+	if err != nil {
+		return nil, fmt.Errorf("failed to index overlay file %q: %w", path, err)
+	}
+	f, err := rolo.Open(filepath.Base(filePath))
+	if err != nil {
+		return nil, fmt.Errorf("failed to open overlay file %q: %w", path, err)
 	}
 
-	resolverRef := resolver.NewResolver(idx)
-	resolvingErrors := resolverRef.Resolve()
-	// any errors found during resolution? Print them out.
-	if len(resolvingErrors) > 0 {
-		msg := ""
-		for _, err := range resolvingErrors {
-			msg += err.Error() + ";"
-		}
-		return nil, fmt.Errorf("error resolving spec: %s", msg)
+	rolo.Resolve()
+	errs := rolo.GetCaughtErrors()
+	if len(errs) > 0 {
+		return nil, fmt.Errorf("failed to resolve overlay file %q: %v", path, errs)
 	}
 
-	err = idx.GetRootNode().Decode(&overlay)
+	var overlay Overlay
+	resolver := f.GetIndex().GetResolver()
+	resolvingErrs := resolver.Resolve()
+	if len(resolvingErrs) > 0 {
+		return nil, fmt.Errorf("failed to resolve overlay file %q: %v", path, resolvingErrs)
+	}
+	rootNode := f.GetIndex().GetRootNode()
+
+	err = rootNode.Decode(&overlay)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get content of overlay file %q: %w", path, err)
+	}
+	//err = rolo.GetContentAsYAMLNode().Decode(&overlay)
 	if err != nil {
 		return nil, err
 	}
